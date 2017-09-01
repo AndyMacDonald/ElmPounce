2bdd943666e1ca6e13e938cdc26f1e5290aa8fef
Andrew MacDonald <awm@alum.mit.edu>
2017-08-31 22:40:05 -0700

    Render a grid

--- src/Board.elm
+++ src/Board.elm
@@ -16,21 +16,35 @@ type Square
 -- The board
 -- This first implementation is very squishy with too many degrees of 
 -- freedom. Tighten up later.
-type Board =
-    Board
-        { squares : Array (Array Square)
-        , next : Player
-        }
+type alias Board =
+    { squares : Array (Array Square)
+    , next : Player
+    }
 
 initialBoard : Board
 initialBoard =
-    Board
-        { squares = Array.repeat 7 (Array.repeat 7 Empty)
-        , next = X
-        }
+    { squares = Array.repeat 7 (Array.repeat 7 Empty)
+    , next = X
+    }
 
 render : Board -> Html.Html msg
 render board = 
-    svg [ viewBox "0 0 70 70", width "300px" ]
-      [ circle [ cx "35", cy "35", r "30", fill "#0B79CE" ] []
-      ]
+    svg [ viewBox "0 0 72 72", width "300px" ]
+      (Array.indexedMap renderRow board.squares |> foldl List.append [])
+
+renderRow : Int -> Array Square -> List (Svg msg)
+renderRow idx row =
+    (Array.indexedMap (renderColumn idx) row |> Array.toList)
+
+renderColumn : Int -> Int -> Square -> Svg msg
+renderColumn yidx xidx square =
+    let
+        xpos = toString (10 * xidx + 1)
+        ypos = toString (10 * yidx + 1)
+        color = case square of
+            Empty -> "white"
+            Blocked -> "black"
+            
+    in
+        rect [x xpos, y ypos, width "10", height "10", stroke "black", strokeWidth "1", fill color] []
+            
