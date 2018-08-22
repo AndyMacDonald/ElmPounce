4317eb67d5d8fc9221f2bf8bc5dd088e2e81406b
Andrew MacDonald <awm@alum.mit.edu>
2018-08-22 09:52:50 -0700

    Reformat code using elm formatter

--- src/Board.elm
+++ src/Board.elm
@@ -1,6 +1,13 @@
-module Board exposing
-    (Model, init, view, Msg(Clicked), update, statusText,
-        Player(XMove, OMove, XBlocked, OBlocked, XPounced, OPounced))
+module Board
+    exposing
+        ( Model
+        , init
+        , view
+        , Msg(..)
+        , update
+        , statusText
+        , Player(XMove, OMove, XBlocked, OBlocked, XPounced, OPounced)
+        )
 
 import Set exposing (..)
 import Html exposing (Html)
@@ -8,20 +15,26 @@ import Svg exposing (..)
 import Svg.Attributes exposing (..)
 import Svg.Events exposing (..)
 
+
 type Player
-  = XMove
-  | OMove
-  | XBlocked
-  | OBlocked
-  | XPounced
-  | OPounced
+    = XMove
+    | OMove
+    | XBlocked
+    | OBlocked
+    | XPounced
+    | OPounced
+
 
 type Msg
     = Clicked Int
 
+
+
 -- The board
--- This first implementation is very squishy with too many degrees of 
+-- This first implementation is very squishy with too many degrees of
 -- freedom. Tighten up later.
+
+
 type alias Model =
     { blocked : Set Int
     , xpos : Int
@@ -29,6 +42,7 @@ type alias Model =
     , moves : Set Int
     , next : Player
     }
+
 
 init : Model
 init =
@@ -39,27 +53,41 @@ init =
     , next = XMove
     }
 
+
 view : Model -> Html Msg
-view model = 
+view model =
     svg [ viewBox "0 0 72 72", width "300px" ]
         (List.map (renderSquare model) allSquares)
+
 
 renderSquare : Model -> Int -> Svg Msg
 renderSquare model idx =
     let
-        (xidx, yidx) = idxToXY(idx)
-        xpos = toString (10 * xidx + 1)
-        ypos = toString (10 * yidx + 1)
-        color = squareColor model idx
-        handler = makeHandler model idx
-        attributes = [x xpos, y ypos, width "10", height "10", stroke "black", strokeWidth "1", fill color]
+        ( xidx, yidx ) =
+            idxToXY (idx)
 
+        xpos =
+            toString (10 * xidx + 1)
+
+        ypos =
+            toString (10 * yidx + 1)
+
+        color =
+            squareColor model idx
+
+        handler =
+            makeHandler model idx
+
+        attributes =
+            [ x xpos, y ypos, width "10", height "10", stroke "black", strokeWidth "1", fill color ]
     in
         case handler of
-        Just callback ->
-            rect (callback :: attributes) []
-        Nothing ->
-            rect attributes []
+            Just callback ->
+                rect (callback :: attributes) []
+
+            Nothing ->
+                rect attributes []
+
 
 update : Msg -> Model -> Model
 update msg model =
@@ -68,19 +96,28 @@ update msg model =
             case model.next of
                 XMove ->
                     let
-                        newNext = OMove
-                        from = model.xpos
-                        newModel = 
+                        newNext =
+                            OMove
+
+                        from =
+                            model.xpos
+
+                        newModel =
                             { blocked = insert from model.blocked
                             , xpos = idx
                             , opos = model.opos
                             , moves = empty
                             , next = newNext
                             }
-                        myMoves = Set.filter (legal newModel newModel.xpos)
-                                             (Set.diff (Set.fromList allSquares) newModel.blocked)
-                        newMoves = Set.filter (legal newModel newModel.opos)
-                                             (Set.diff (Set.fromList allSquares) newModel.blocked)
+
+                        myMoves =
+                            Set.filter (legal newModel newModel.xpos)
+                                (Set.diff (Set.fromList allSquares) newModel.blocked)
+
+                        newMoves =
+                            Set.filter (legal newModel newModel.opos)
+                                (Set.diff (Set.fromList allSquares) newModel.blocked)
+
                         realNext =
                             if idx == model.opos then
                                 XPounced
@@ -92,22 +129,31 @@ update msg model =
                                 OMove
                     in
                         { newModel | next = realNext, moves = newMoves }
-                            
-                OMove -> 
-                     let
-                        newNext = XMove
-                        from = model.opos
-                        newModel = 
+
+                OMove ->
+                    let
+                        newNext =
+                            XMove
+
+                        from =
+                            model.opos
+
+                        newModel =
                             { blocked = insert from model.blocked
                             , xpos = model.xpos
                             , opos = idx
                             , moves = empty
                             , next = newNext
                             }
-                        myMoves = Set.filter (legal newModel newModel.opos) 
-                                             (Set.diff (Set.fromList allSquares) newModel.blocked)
-                        newMoves = Set.filter (legal newModel newModel.xpos)
-                                             (Set.diff (Set.fromList allSquares) newModel.blocked)
+
+                        myMoves =
+                            Set.filter (legal newModel newModel.opos)
+                                (Set.diff (Set.fromList allSquares) newModel.blocked)
+
+                        newMoves =
+                            Set.filter (legal newModel newModel.xpos)
+                                (Set.diff (Set.fromList allSquares) newModel.blocked)
+
                         realNext =
                             if idx == model.xpos then
                                 OPounced
@@ -119,60 +165,96 @@ update msg model =
                                 XMove
                     in
                         { newModel | next = realNext, moves = newMoves }
-                _ -> model
+
+                _ ->
+                    model
+
 
 statusText : Model -> String
 statusText model =
     case model.next of
-        XMove -> "Blue to move"
-        OMove -> "Red to move"
-        XBlocked -> "Blue is trapped. Red wins!"
-        OBlocked -> "Red is trapped. Blue wins!"
-        XPounced -> "Blue pounced on red. Blue wins!"
-        OPounced -> "Red pounced on blue. Red wins!"
+        XMove ->
+            "Blue to move"
+
+        OMove ->
+            "Red to move"
+
+        XBlocked ->
+            "Blue is trapped. Red wins!"
+
+        OBlocked ->
+            "Red is trapped. Blue wins!"
+
+        XPounced ->
+            "Blue pounced on red. Blue wins!"
+
+        OPounced ->
+            "Red pounced on blue. Red wins!"
+
+
 
 -- HELPER FUNCTIONS
-
 -- UPDATE HELPERS
+
 
 legal : Model -> Int -> Int -> Bool
 legal model from to =
     let
-        (fx, fy) = idxToXY from
-        (tx, ty) = idxToXY to
-        (dx, dy) = (tx - fx, ty - fy)
+        ( fx, fy ) =
+            idxToXY from
 
+        ( tx, ty ) =
+            idxToXY to
+
+        ( dx, dy ) =
+            ( tx - fx, ty - fy )
     in
-        if from == -1 then -- First move for each side is a special case
-            (if to == model.xpos then -- can move anywhere except O not allow on top of X
-                False 
+        if from == -1 then
+            -- First move for each side is a special case
+            (if to == model.xpos then
+                -- can move anywhere except O not allow on top of X
+                False
              else
-                True)
+                True
+            )
         else if from == to then
-            False -- Can't move to self
-        else if dx /= 0 && dy /= 0 && abs(dx) /= abs(dy) then
-            False -- Must move like a queen in chess
+            False
+            -- Can't move to self
+        else if dx /= 0 && dy /= 0 && abs (dx) /= abs (dy) then
+            False
+            -- Must move like a queen in chess
         else
             -- No blockers between from and to
             not (blocked model from to (Basics.max (abs dx) (abs dy)))
 
+
 blocked : Model -> Int -> Int -> Int -> Bool
 blocked model from to count =
     let
-        delta = to - from
-        dstep = delta // count -- should be integral
-        idxs = List.map (\x -> from + x * dstep) (List.range 1 count)
+        delta =
+            to - from
 
+        dstep =
+            delta // count
+
+        -- should be integral
+        idxs =
+            List.map (\x -> from + x * dstep) (List.range 1 count)
     in
         List.any (\x -> member x model.blocked) idxs
 
+
+
 -- VIEW HELPERS
+
+
 makeHandler : Model -> Int -> Maybe (Svg.Attribute Msg)
 makeHandler model to =
     if Set.member to model.moves then
         Just (Svg.Events.onClick (Clicked to))
     else
         Nothing
+
 
 squareColor : Model -> Int -> String
 squareColor model idx =
@@ -185,6 +267,7 @@ squareColor model idx =
     else
         "white"
 
+
 xOrPouncedColor : Player -> Int -> Int -> String
 xOrPouncedColor player xpos opos =
     if xpos == opos then
@@ -195,12 +278,16 @@ xOrPouncedColor player xpos opos =
     else
         "#0000DD"
 
+
+
 -- GENERAL HELPERS
+
 
 allSquares : List Int
 allSquares =
     List.range 0 48
 
-idxToXY : Int -> (Int, Int)
+
+idxToXY : Int -> ( Int, Int )
 idxToXY idx =
-    (idx % 7, idx // 7)
+    ( idx % 7, idx // 7 )
--- src/game.elm
+++ src/game.elm
@@ -1,3 +1,5 @@
+module Main exposing (..)
+
 import Html exposing (Html, div, h2, text, button, map, fieldset, label, input)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
@@ -7,119 +9,161 @@ import Task exposing (..)
 import Process exposing (..)
 import Time exposing (..)
 
+
 main : Program Never Model Msg
 main =
-  Html.program
-    { init = init
-    , view = view
-    , update = update
-    , subscriptions = subscriptions
-    }
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
 
 -- MODEL
+
+
 type Opponent
-  = Human
-  | Robot
+    = Human
+    | Robot
+
 
 type alias Model =
-  { board : Board.Model
-  , opponent : Opponent
-  }
+    { board : Board.Model
+    , opponent : Opponent
+    }
+
 
 type Msg
-  = Board Board.Msg
-  | Reset
-  | Opponent Opponent
-  | RobotMove Index
-  | Hold
+    = Board Board.Msg
+    | Reset
+    | Opponent Opponent
+    | RobotMove Index
+    | Hold
 
-init : (Model, Cmd Msg)
+
+init : ( Model, Cmd Msg )
 init =
-  (Model Board.init Robot, Cmd.none)
+    ( Model Board.init Robot, Cmd.none )
+
+
 
 -- VIEW
 
+
 view : Model -> Html Msg
 view model =
-  div []
-    [ h2 [] [text "Pounce"]
-    , Html.p [] [text (statusText model.board)]
-    , Html.map (\a -> Board a) (Board.view model.board)
-    , fieldset []
-      [ radio "One player" (model.opponent == Robot) (Opponent Robot)
-      , radio "Two player" (model.opponent == Human) (Opponent Human)
-      ]
-    , button [ onClick Reset ] [ text "Reset" ]
-    ]
+    div []
+        [ h2 [] [ text "Pounce" ]
+        , Html.p [] [ text (statusText model.board) ]
+        , Html.map (\a -> Board a) (Board.view model.board)
+        , fieldset []
+            [ radio "One player" (model.opponent == Robot) (Opponent Robot)
+            , radio "Two player" (model.opponent == Human) (Opponent Human)
+            ]
+        , button [ onClick Reset ] [ text "Reset" ]
+        ]
+
 
 radio : String -> Bool -> msg -> Html msg
 radio value isChecked msg =
-  label
-    [ style [("padding", "20px")]
-    ]
-    [ input [ type_ "radio", onClick msg, checked isChecked ] []
-    , text value
-    ]
+    label
+        [ style [ ( "padding", "20px" ) ]
+        ]
+        [ input [ type_ "radio", onClick msg, checked isChecked ] []
+        , text value
+        ]
+
+
 
 -- UPDATE
 
-update : Msg -> Model -> (Model, Cmd Msg)
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    Board boardMsg ->
-        case model.opponent of
-            Human -> ({model | board = Board.update boardMsg model.board}, Cmd.none)
-            Robot ->
-                -- Eat clicks while the robot is running
-                if model.board.next == OMove then
-                    (model, Cmd.none)
-                else
-                    let
-                        humanMove = Board.update boardMsg model.board
-                    in
-                        ({model | board = humanMove}, runRobot humanMove)
+    case msg of
+        Board boardMsg ->
+            case model.opponent of
+                Human ->
+                    ( { model | board = Board.update boardMsg model.board }, Cmd.none )
 
-    Reset ->
-      (Model Board.init model.opponent, Cmd.none)
+                Robot ->
+                    -- Eat clicks while the robot is running
+                    if model.board.next == OMove then
+                        ( model, Cmd.none )
+                    else
+                        let
+                            humanMove =
+                                Board.update boardMsg model.board
+                        in
+                            ( { model | board = humanMove }, runRobot humanMove )
 
-    Opponent opponent ->
-      (Model Board.init opponent, Cmd.none)
+        Reset ->
+            ( Model Board.init model.opponent, Cmd.none )
 
-    RobotMove idx ->
-        ({model | board = Board.update (Clicked idx) model.board}, Cmd.none)
+        Opponent opponent ->
+            ( Model Board.init opponent, Cmd.none )
 
-    Hold ->
-        (model, Cmd.none)
+        RobotMove idx ->
+            ( { model | board = Board.update (Clicked idx) model.board }, Cmd.none )
+
+        Hold ->
+            ( model, Cmd.none )
+
+
 
 -- SUBSCRIPTIONS
 
+
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Sub.none
+    Sub.none
 
 
 
 ----------------------
 -- NEGAMAX code for a robot to play against
 
-type alias Index = Int
-type alias Score = Int
-type alias Color = Int
-type alias Budget = Int
+
+type alias Index =
+    Int
+
+
+type alias Score =
+    Int
+
+
+type alias Color =
+    Int
+
+
+type alias Budget =
+    Int
+
 
 runRobot : Board.Model -> Cmd Msg
 runRobot board =
     Task.perform
-        (\a -> case a of
-            Just idx -> RobotMove idx
-            Nothing -> Hold)
+        (\a ->
+            case a of
+                Just idx ->
+                    RobotMove idx
+
+                Nothing ->
+                    Hold
+        )
         (Task.andThen
             (\_ -> Task.succeed (nextMove board))
-            (Process.sleep (50 * Time.millisecond)))
+            (Process.sleep (50 * Time.millisecond))
+        )
+
 
 nextMove : Board.Model -> Maybe Index
 nextMove board =
     negamax scorer board 2000 (win * -2) (win * 2) 1 |> Tuple.second
+
+
 
 -- scoreFn returns board score from O's viewpoint
 -- board is current node in the tree
@@ -128,48 +172,71 @@ nextMove board =
 -- beta is the uppor bound of child node values at this depth
 -- color is 1 for O and -1 for X (robot is always O)
 -- returns pair of (best score, number of square for that score)
-negamax : (Board.Model -> Score) -> Board.Model -> Budget -> Score -> Score -> Color -> (Score, Maybe Index)
+
+
+negamax : (Board.Model -> Score) -> Board.Model -> Budget -> Score -> Score -> Color -> ( Score, Maybe Index )
 negamax scoreFn board budget alpha beta color =
-  if budget <= 0 || Set.isEmpty board.moves then
-    (color * scoreFn board, Nothing)
-  else
-    let
-        candidates = List.map (\x -> (x, (Board.update (Clicked x) board))) (Set.toList board.moves)
-        newBudget = budget // (Set.size board.moves) - 1
-    in
-        alphaBetaPruner scoreFn candidates newBudget alpha beta color (-1000 * win, Nothing)
+    if budget <= 0 || Set.isEmpty board.moves then
+        ( color * scoreFn board, Nothing )
+    else
+        let
+            candidates =
+                List.map (\x -> ( x, (Board.update (Clicked x) board) )) (Set.toList board.moves)
+
+            newBudget =
+                budget // (Set.size board.moves) - 1
+        in
+            alphaBetaPruner scoreFn candidates newBudget alpha beta color ( -1000 * win, Nothing )
 
 
-alphaBetaPruner : (Board.Model -> Score) -> List (Index, Board.Model) -> Budget -> Score -> Score -> Color -> (Score, Maybe Index) -> (Score, Maybe Index)
+alphaBetaPruner : (Board.Model -> Score) -> List ( Index, Board.Model ) -> Budget -> Score -> Score -> Color -> ( Score, Maybe Index ) -> ( Score, Maybe Index )
 alphaBetaPruner scoreFn boards budget alpha beta color best =
     case boards of
-        (idx, board) :: rest ->
+        ( idx, board ) :: rest ->
             let
-                (negScore, _) = negamax scorer board budget -beta -alpha -color
-                newBest = 
+                ( negScore, _ ) =
+                    negamax scorer board budget -beta -alpha -color
+
+                newBest =
                     if Tuple.first best >= -negScore then
                         best
                     else
-                        (-negScore, Just idx)
-                newAlpha = Basics.max alpha -negScore
+                        ( -negScore, Just idx )
+
+                newAlpha =
+                    Basics.max alpha -negScore
             in
                 if newAlpha < beta then
                     alphaBetaPruner scoreFn rest budget newAlpha beta color newBest
                 else
                     newBest
 
-        [] -> best
+        [] ->
+            best
+
 
 scorer : Board.Model -> Score
 scorer board =
-  case board.next of
-    XMove -> -(Set.size board.moves)
-    OMove -> Set.size board.moves
-    XPounced -> -win
-    OPounced -> win
-    XBlocked -> win
-    OBlocked -> -win
+    case board.next of
+        XMove ->
+            -(Set.size board.moves)
+
+        OMove ->
+            Set.size board.moves
+
+        XPounced ->
+            -win
+
+        OPounced ->
+            win
+
+        XBlocked ->
+            win
+
+        OBlocked ->
+            -win
+
 
 win : Score
 win =
-  1000000
+    1000000
